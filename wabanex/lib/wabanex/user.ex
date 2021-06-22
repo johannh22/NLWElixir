0064e6dbcce982c1e63f8cbb9264f652da4b261a
johannh22 <johannhomonnai@gmail.com>
2021-06-22 16:34:05 -0300

    Validations for user

---
+++ wabanex/lib/wabanex/user.ex
@@ -0,0 +1,24 @@
+defmodule Wabanex.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @fields [:email, :name, :password]
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields) # this says that @fields are required for validation
+    |> validate_length(:password, min: 6)
+    |> validate_length(:name, min: 2)
+    |> validate_format(:email, ~r/@/) # email must have an @
+    |> unique_constraint([:email]) # validate if email is unique
+  end
+end

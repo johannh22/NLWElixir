274b0d0a5c008a1256cf54e3bf5ddf6faa680e87
johannh22 <johannhomonnai@gmail.com>
2021-06-22 16:44:26 -0300

    Now user is generating id automatically

--- wabanex/lib/wabanex/user.ex
+++ wabanex/lib/wabanex/user.ex
@@ -2,6 +2,8 @@ defmodule Wabanex.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+
   @fields [:email, :name, :password]
 
   schema "users" do
